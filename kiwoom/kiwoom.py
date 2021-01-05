8bbf83098fa88bded60e6aba7f1eebdf9f87c3be
Chaegeon <thscorjs1002@naver.com>
2021-01-05 16:02:54 +0900

    [UPDATE] get kiwoom disposit

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -17,17 +17,23 @@ class Kiwoom(QAxWidget):
         self.account_num = None
         ##########
 
+        # 이벤트 루프 모음
+        ################
+        self.detail_account_event_loop = None
+
         self.get_ocx_instance()
         self.event_slots()
 
         self.signal_login_commConnect()
-        self.get_account_info()
+        self.get_account_info() # 계좌번호 가져오기
+        self.detail_account_info() # 예수금 가져오기
 
     def get_ocx_instance(self):
         self.setControl('KHOPENAPI.KHOpenAPICtrl.1')
 
     def event_slots(self):
         self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.trdata_slot)
 
     def signal_login_commConnect(self):
         self.dynamicCall("CommConnect()")
@@ -36,9 +42,7 @@ class Kiwoom(QAxWidget):
         self.login_event_loop.exec_()
 
     def login_slot(self, errCode):
-        print(errCode)
         print(errors(errCode))
-
 
         self.login_event_loop.exit()
 
@@ -47,3 +51,33 @@ class Kiwoom(QAxWidget):
 
         self.account_num = account_list.split(';')[0] # '12312323;' TO [12312323]
         print('나의 보유 계좌번호 %s ' % self.account_num)
+
+    def detail_account_info(self):
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
+
+        self.detail_account_event_loop = QEventLoop()
+        self.detail_account_event_loop.exec_()
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        '''
+        TR요청을 받는 슬롯
+        :param sScrNo: 스크린번호
+        :param sRQName: 요청했을 때 이름
+        :param sTrCode:
+        :param sRecordName: 사용 안함
+        :param sPrevNext: 다음 페이지가 있는지
+        :return:
+        '''
+
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(String, String, String, String)", sTrCode, sRQName, 0, "예수금")
+            print("예수금 %s" % int(deposit))
+
+            ok_deposit = self.dynamicCall("GetCommData(String, String, String, String)", sTrCode, sRQName, 0, "출금가능금액")
+            print("출금가능금액 %s" % int(ok_deposit))
+
+            self.detail_account_event_loop.exit()
