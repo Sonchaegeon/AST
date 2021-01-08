3ac0815db1ea2bbedc6d252cfa1004bfdd053072
Chaegeon <thscorjs1002@naver.com>
2021-01-08 20:12:11 +0900

    [UPDATE] get buy_money, profit_rate

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -11,15 +11,13 @@ class Kiwoom(QAxWidget):
 
         # eventLoop 모음
         self.login_event_loop = None
+        self.detail_account_event_loop = None
+        self.detail_mystock_event_loop = None
         ###############
 
         # 변수 모음
         self.account_num = None
         ##########
-
-        # 이벤트 루프 모음
-        ################
-        self.detail_account_event_loop = None
 
         self.get_ocx_instance()
         self.event_slots()
@@ -27,6 +25,7 @@ class Kiwoom(QAxWidget):
         self.signal_login_commConnect()
         self.get_account_info() # 계좌번호 가져오기
         self.detail_account_info() # 예수금 가져오기
+        self.detail_account_mystock() # 계좌평가잔고 가져오기
 
     def get_ocx_instance(self):
         self.setControl('KHOPENAPI.KHOpenAPICtrl.1')
@@ -62,6 +61,18 @@ class Kiwoom(QAxWidget):
         self.detail_account_event_loop = QEventLoop()
         self.detail_account_event_loop.exec_()
 
+    def detail_account_mystock(self, sPrevNext="0"):
+        print("계좌평가잔고내역요청")
+
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, Int, String)", "계좌평가잔고내역요청", "opw00018", sPrevNext, "2000")
+
+        self.detail_mystock_event_loop = QEventLoop()
+        self.detail_mystock_event_loop.exec_()
+
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
         '''
         TR요청을 받는 슬롯
@@ -81,3 +92,16 @@ class Kiwoom(QAxWidget):
             print("출금가능금액 %s" % int(ok_deposit))
 
             self.detail_account_event_loop.exit()
+
+        if sRQName == "계좌평가잔고내역요청":
+            total_buy_money = self.dynamicCall("GetCommData(String, String, Int, String", sTrCode, sRQName, 0, "총매입금액")
+            total_buy_money_result = int(total_buy_money)
+
+            print("총매입금액 %s" % total_buy_money_result)
+
+            total_profit_rate = self.dynamicCall("GetCommData(String, String, Int, String", sTrCode, sRQName, 0, "총수익률(%)")
+            total_profit_result = float(total_profit_rate)
+
+            print("총 수익률(%%) : %s" % total_profit_result)
+
+            self.detail_mystock_event_loop.exit()
